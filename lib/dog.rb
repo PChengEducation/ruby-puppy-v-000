efb77a8257afe66b05e08b4c9f78c9e5b539387e
Pan Cheng <pchengeducation@hotmail.com>
2018-07-14 22:24:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,13 +5,15 @@ def initialize(name)
   @@all = []
 end
 
-
-
-
 def self.clear_all
   @@all.each do |dog|
     dog.delete
   end
 end
+
+def self.all
+  puts @@all
+end
+
 
 end # end of the Dog class
